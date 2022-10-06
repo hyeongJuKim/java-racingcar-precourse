d06d75a04be52e80f3bead52e327cab318ad3e2b
hyeongju <gudwndhkd@nate.com>
2022-10-06 21:51:19 +0900

    refactor(RacingCar): 검증 함수 생성
    null 값과 0-5자리 검증하는 함수를 생성

--- src/main/java/racingcar/model/RacingCar.java
+++ src/main/java/racingcar/model/RacingCar.java
@@ -1,24 +1,29 @@
 package racingcar.model;
 
 public class RacingCar {
-    private String name;
     private int forwardCount = 0;
+    private String name;
 
     public RacingCar(String name) {
-
-        if(name == null){
-            throw new IllegalArgumentException("[ERROR] 자동차의 이름을 입력해주세요.");
-        }
-
-        if(name.length() > 5){
-            throw new IllegalArgumentException("[ERROR] 자동차의 이름은 1-5자 이어야합니다. 입력한 이름 길이(" + name.length() + ")");
-        }
-
+        validNull(name);
+        validLength(name);
         this.name = name;
     }
 
     public String getName(){
         return this.name;
+    }
+
+    private void validNull(String name){
+        if(name == null){
+            throw new IllegalArgumentException("[ERROR] 자동차의 이름을 입력해주세요.");
+        }
+    }
+
+    private void validLength(String name){
+        if(name.length() == 0 || name.length() > 5){
+            throw new IllegalArgumentException("[ERROR] 자동차의 이름은 1-5자 이어야합니다. 입력한 이름 길이(" + name.length() + ")");
+        }
     }
 
     public int getForwardCount() {
@@ -27,5 +32,13 @@ public class RacingCar {
 
     public int forwardCount() {
         return this.forwardCount++;
+    }
+
+    @Override
+    public String toString() {
+        return "RacingCar{" +
+                "forwardCount=" + forwardCount +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
